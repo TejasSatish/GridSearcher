f7e1494bbb262a22a339408604bca1a82b5e11e5
TejasSatish <tejassatish@gmail.com>
2022-12-30 14:25:12 +0530

    drawing walls added

--- js/EventListeners.js
+++ js/EventListeners.js
@@ -2,7 +2,7 @@ var mouse={ //mouse object
     x:undefined,
     y:undefined
 }
-
+var drawWallMode=0;
 window.addEventListener('mousemove', //tracks mouse x,y
     function(event){
         mouse.x=event.x;
@@ -11,16 +11,30 @@ window.addEventListener('mousemove', //tracks mouse x,y
 );
 
 let board=document.getElementById('board');
-board.addEventListener("mousedown",function(event){
-    if(event.target.tagName==='TD'){
-        let currentNodeStatus=event.target.className;
-        //let currentNode=newBoard.getNode(currentNodeId);
-        if(currentNodeStatus==='unvisited'){
-            event.target.className='wall';
-        }else if(currentNodeStatus==='wall'){
-            event.target.className='unvisited';
+
+board.addEventListener("mousemove",  //to draw walls
+    function(event){
+        if(event.target.tagName==='TD' && drawWallMode===1){
+            drawWallMode=1;
+            let currentNodeStatus=event.target.className;
+            //let currentNode=newBoard.getNode(currentNodeId);
+            if(currentNodeStatus==='unvisited'){
+                event.target.className='wall';
+            }else if(currentNodeStatus==='wall'){
+                event.target.className='unvisited';
+            }
+        }else{
+            
         }
     }
-}
+);
+board.addEventListener("mousedown", //to enter drawmode
+    function(event){
+        if(drawWallMode===0){
+            drawWallMode=1;
+        }else{
+            drawWallMode=0;
+        }
+    }
 );
 
