73d316a9dd08e88d6b6c0928300ac6a44749e6d7
TejasSatish <tejassatish@gmail.com>
2023-01-06 03:04:00 +0530

    dijstra's over, need to show shortest path

--- js/Algorithms.js
+++ js/Algorithms.js
@@ -9,16 +9,29 @@ function getAllNodes(){
     }
     return allNodes;
 }
-function dijkstra(){
+async function dijkstra(){
     let start=newBoard.getNode(newBoard.src);
     let end=newBoard.getNode(newBoard.sink);
     newBoard.getNode(newBoard.src).distance=0;
-    
     const unvisited=getAllNodes();
-    while(0<unvisited.length){
-        console.log("In loop");
+    while(!!unvisited.length){
+        await sleep(10); //timeout fn for clearer viz
+        
         sortByDist(unvisited);
         const closest=unvisited.shift();
+        if(closest.distance===9999){
+            return path;
+        }
+        let closestId=closest.id;
+        let close=document.getElementById(closestId);
+        if(closest === start || closest === end){
+            
+        }else if(close.className === "wall"){
+            continue;
+        }else{
+            close.className="visited"; 
+        }
+        
         closest.isVisited=true;
         path.push(closest);
         if(closest === end){
@@ -29,23 +42,22 @@ function dijkstra(){
     }
 
 }
-
+function sleep(ms){
+    return new Promise(resolve=>setTimeout(resolve,ms));
+}
 function sortByDist(unvisited){ //sorts in descending order. Need to make it sort in asc
-    console.log("sorting unvisited");
     unvisited.sort((node1, node2)=>{
         if(node1.distance<node2.distance){
-            return 1;
-        }else if(node1.distance>node2.distance){
             return -1;
+        }else if(node1.distance>node2.distance){
+            return 1;
         }
         return 0;
     }); //sorts the array in ascendign
-    console.log(unvisited)
 }
 
 //updates only unvisited neighbours
 function updateNeighbours(node){
-    console.log("updating neighbours");
     const unvisitedNeighbours=getNeighbours(node);
 
     for(const neighbour of unvisitedNeighbours){
@@ -55,23 +67,25 @@ function updateNeighbours(node){
 }
 
 function getNeighbours(node){
-    const neighbours=[];
+    var neighbours=[];
     let nodeId=newBoard.getId(node);
     let row=nodeId.i;
     let column=nodeId.j;
+    if(row>0){ //up neighbour
+        neighbours.push(newBoard.nodeArray[row-1][column]);
+    }
     if(column>0){ //left neighbour
         neighbours.push(newBoard.nodeArray[row][column-1]);
+    }
+    if(row<newBoard.height-1){ //down neighbour
+        neighbours.push(newBoard.nodeArray[row+1][column]);
     }
     if(column<newBoard.width-1){ //right neighbour
         neighbours.push(newBoard.nodeArray[row][column+1]);
     }
-    if(row>0){ //above neighbour
-        neighbours.push(newBoard.nodeArray[row-1][column]);
-    }
-    if(row<newBoard.height-1){ //below neighbour
-        neighbours.push(newBoard.nodeArray[row+1][column]);
-    }
+    
+    
 
-    return neighbours.filter(neighbour=>{ !neighbour.isVisited});
+    return neighbours;
 }
 
